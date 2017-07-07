123b4c9f29f4d8c23143029a7cec9a178367f62a
Jothi Ramaswamy <jothiramaswamy@thinksteam4girls.org>
2017-07-07 12:56:44 -0400

    first commit

--- Sound Shaker/ViewController.swift
+++ Sound Shaker/ViewController.swift
@@ -7,8 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    
+    var player = AVAudioPlayer()
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        let soundArray = ["chimpanzee", "cricket", "dog1", "squirrel", "toad"]
+        
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+            
+            let random = Int(arc4random_uniform(UInt32(soundArray.count)))
+            
+            let audioPath = Bundle.main.path(forResource: soundArray[random], ofType: "mp3")
+            
+            
+            
+            do {
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+                
+                player.play()
+            } catch {
+                
+            }
+            
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
